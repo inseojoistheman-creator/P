19c953d61087227423271b109adb6621ab8ac324
inseojoistheman-creator <inseojoistheman@gmail.com>
2026-09-20 18:26:41 +0900

    ㅇㅇㅇ

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 같은 github.io 주소의 다른 앱 캐시는 지우지 않도록 'p-'로 시작하는 캐시만 정리한다.
-const CACHE='p-v6';
+const CACHE='p-v7';
 const CORE=["./altar.html", "./altar.webmanifest", "./altar-icon-180.png", "./altar-icon-512.png"];
 self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE).then(async c=>{ for(const u of CORE){ try{ await c.add(u); }catch(err){} } self.skipWaiting(); })); });
 self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k.startsWith('p-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
